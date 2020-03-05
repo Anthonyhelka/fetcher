058f55b9c46dcdfbb72bd3cad1b5ab7201243af0
Anthonyhelka <helkaanthony1997@gmail.com>
2020-03-04 22:25:03 -0500

    Removed client cache.

---
+++ client/src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  state = {
+    products: []
+  };
+
+  componentDidMount() {
+    this.callBackendAPI()
+      .then(res => this.setState({ products: res.products }))
+      .catch(err => console.log(err));
+  }
+
+  callBackendAPI = async () => {
+    const response = await fetch('/api/products');
+    const body = await response.json();
+    if (response.status !== 200) {
+      throw Error(body.message)
+    }
+    return body;
+  };
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.products.map((product) => {
+          return(
+            <p key={product.label}>{product.label}, ${product.price}</p>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default App;
