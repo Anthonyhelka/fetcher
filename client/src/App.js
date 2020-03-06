2b4e668cf65d78b58712dae0d46bec9c2fbc4b94
Anthonyhelka <helkaanthony1997@gmail.com>
2020-03-05 20:02:29 -0500

    Removed loadingImage conditional.

--- client/src/App.js
+++ client/src/App.js
@@ -9,8 +9,7 @@ class App extends Component {
     breeds: [],
     selectedBreed: 'All Breeds',
     randomDog: '',
-    fetching: false,
-    imageLoading: true
+    loading: false
   };
 
   componentDidMount() {
@@ -19,33 +18,28 @@ class App extends Component {
   }
 
   getBreeds = () => {
-    this.setState({ fetching: true });
     fetch('/api/breeds')
-      .then((response) => {
-        return response.json();
-       })
-       .then((body) => {
-         this.setState({ breeds: body, fetching: false });
-       })
+    .then((response) => {
+      return response.json();
+     })
+     .then((body) => {
+       this.setState({ breeds: body });
+     })
   };
 
   getRandomDog = () => {
-    this.setState({ fetching: true });
+    this.setState({ loading: true });
     fetch(`/api/randomDog?breed=${this.state.selectedBreed}`)
     .then((response) => {
       return response.json();
      })
      .then((body) => {
-       this.setState({ randomDog: body.message, fetching: false, imageLoading: true });
+       this.setState({ randomDog: body.message, loading: false });
      })
   };
 
   handleBreedChange = (event) => {
     this.setState({ selectedBreed: event.target.value });
-  }
-
-  handleImageLoad = () => {
-    this.setState({ imageLoading: false });
   }
 
   render() {
@@ -54,8 +48,7 @@ class App extends Component {
         <h1 id='title'>Fetcher</h1>
         <button id='fetch-button' onClick={this.getRandomDog}>Fetch!</button>
         <BreedSelect breeds={this.state.breeds} handleBreedChange = {(event) => this.handleBreedChange(event)} />
-        {this.state.fetching || this.state.imageLoading && <img id='loading-gears' src={loading_gears} alt='Loading Gears' />}
-        <img id='dog-image' src={this.state.randomDog} style={{visibility: this.state.imageLoading ? 'hidden' : 'visible'}} alt='Random Dog' onLoad={() => this.handleImageLoad()} />
+        {this.state.loading ? <img id='loading-gears' src={loading_gears} alt='Loading Gears' /> : <img id='dog-image' src={this.state.randomDog} alt='Random Dog' />}
       </div>
     );
   }
